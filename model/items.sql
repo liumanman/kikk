dd74bcb5c89459052b1093d19e95757ea1bc2903
liumanman <liumanman@gmail.com>
2016-08-30 00:23:44 -0700

    add items into items.sql

--- model/items.sql
+++ model/items.sql
@@ -127,7 +127,202 @@ select
 	,350
 	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T501_0C6_front.jpg'
 	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-aqua-dash-zou-bisou.html'
-
+union all
+select
+	'T501003'
+	,'JanSport SuperBreak School Backpack - Navy'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_003_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-navy.html'
+union all
+select	
+	'T50101Z'
+	,'JanSport SuperBreak School Backpack - CORAL DUSK TRIBAL MOSAICo'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/14/100/100/T501_01Z_front.jpg'
+	,'JanSport SuperBreak School Backpack - CORAL DUSK TRIBAL MOSAIC'
+union all
+select
+	'T5010BE'
+	,'JanSport SuperBreak School Backpack - GREY RABBIT SYLVIA DOT'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T501_0BE_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-grey-rabbit-sylvia-dot.html'
+union all
+select
+	'T5010D5'
+	,'JanSport SuperBreak School Backpack - Tahitian Orange'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/17/100/100/T501_0D5_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-tahitian-orange.html'
+union all
+select
+	'T5010E2'
+	,'Jansport Superbreak Backpack- Multi Navy Mountain Meadow'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/17/100/100/718W739H3tL._SL1200_.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-backpack-multi-navy-mountain-meadow.html'
+union all
+select
+	'T5010K6'
+	,'JanSport SuperBreak School Backpack - Navy Night Sky'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/17/100/100/T501_0K6_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-navy-night-sky.html'	
+union all
+select
+	'T5010L2'
+	,'Jansport Superbreak Backpack- Multi Wet Sloth'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/17/100/100/wet_sloth_front_.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-backpack-multi-wet-sloth.html'
+union all
+select
+	'T5011Q4'
+	,'JanSport SuperBreak School Backpack - FLUORESCENT RED'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/11/100/100/T5011Q4-1.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-fluorescent-red.html'
+union all
+select
+	'T5012D5'
+	,'JanSport SuperBreak School Backpack - Lorac Yellow'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_2D5_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-lorac-yellow.html'
+union all
+select
+	'T5016XD'
+	,'JanSport SuperBreak School Backpack - Forge Grey'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_6XD_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-forge-grey.html'
+union all
+select
+	'T5019ZG'
+	,'JanSport SuperBreak School Backpack - Aqua Dash'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/11/100/100/T501_9ZG_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-aqua-dash.html'
+union all
+select
+	'T50F5CS'
+	,'JanSport DIGIBREAK BACKPACK - BLUE STREAK'
+	,0
+	,0
+	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T50F_5CS_front.jpg'
+	,'http://www.fantasyard.com/jansport-digibreak-backpack-blue-streak.html'
+union all
+select
+	'TAN19RX'
+	,'JanSport FIFTH AVENUE WAISTPACK - FLUORESCENT PINK'
+	,0
+	,0
+	,'https://www.fantasyard.com/images/thumbnails/13/100/100/TAN1_9RX_front.jpg'
+	,'http://www.fantasyard.com/jansport-fifth-ave-waistpack-fluorescent-pink.html'
+union all
+select
+	'TDN70E9'
+	,'JanSport Big Student School Backpack - MULTI SUNSET STRIPE'
+	,0
+	,0
+	,'https://www.fantasyard.com/images/thumbnails/17/100/100/TDN70E9-1.jpg'
+	,'http://www.fantasyard.com/jansport-big-student-school-backpack-multi-sunset-stripe.html'
+union all
+select
+	'T26L9ER'	
+	,'JanSport ALL PURPOSE Backpack - BLUE WASH'
+	,0
+	,0
+	,'https://www.fantasyard.com/images/thumbnails/13/100/100/T26L_9ER_front.jpg'
+	,'http://www.fantasyard.com/jansport-all-purpose-backpack-blue-wash.html'
+union all
+select
+	'T50109N'
+	,'JanSport SuperBreak School Backpack - MULTI MIXTAPES'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T501_09N_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-multi-mixtapes.html'
+union all
+select
+	'T5010A1'
+	,'JanSport SuperBreak School Backpack - MULTI GREY FLORAL FLOURISH'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T501_0A1_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-multi-grey-floral-flourish.html'
+union all
+select
+	'T5010AA'
+	,'JanSport SuperBreak School Backpack - BLACK LUCKY DAISY'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T501_0AA_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-black-lucky-daisy.html'
+union all
+select
+	'T5010AD'
+	,'JanSport SuperBreak School Backpack - MULTI PURPLE OMBRE DAISY'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/15/100/100/T501_0AD_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-multi-purple-ombre-daisy.html'
+union all
+select
+	'T5015KS'
+	,'JanSport SuperBreak School Backpack - High Risk Red'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_5KS_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-high-risk-red.html'
+union all
+select
+	'T5019FL'
+	,'JanSport SuperBreak School Backpack - Viking Red'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_9FL_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-viking-red.html'
+union all
+	'T5019RW'
+	,'JanSport SuperBreak School Backpack - Mammoth Blue'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_9RW_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-mammoth-blue.html'
+union all
+	'T5019RX'
+	,'JanSport SuperBreak School Backpack - Fluorescent Pink'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_9RX_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-fluorescent-pink.html'
+union all
+	'T5019SA'
+	,'JanSport SuperBreak School Backpack - Coral Peaches'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/12/100/100/T501_9SA_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-coral-peaches.html'
+union all
+	'T501ZE6'
+	,'JanSport SuperBreak School Backpack - GREY TAR WILD AT HEART'
+	,1900
+	,350
+	,'https://www.fantasyard.com/images/thumbnails/11/100/100/T501_ZE6_front.jpg'
+	,'http://www.fantasyard.com/jansport-superbreak-school-backpack-grey-tar-wild-at-heart.html'
 
 
 select
