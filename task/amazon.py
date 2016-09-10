6b19adc0292ab5d7fb697cc1e148592a7acdc8b6
liumanman <liumanman@gmail.com>
2016-09-09 18:32:42 -0700

    working in retrieving price from amazon

--- task/amazon.py
+++ task/amazon.py
@@ -332,12 +332,11 @@ def _adjust_q4s(listing_from_amazon):
     _upload_q4s(changed)
 
 
-def adjust_q4s():
-    listing_list = Listing.query.filter_by(status=Listing.STATUS_OPEN, auto_q4s=1).all()
+def calculate_q4s():
+    listing_list = Listing.query.filter_by(status=Listing.STATUS_OPEN).all()
     all_inventory_data = fantasyard.get_inventory_data()
     _logger.info('item qty in inventory list: {}'.format(len(all_inventory_data)))
     item_qty_in_pending = _get_item_qty_in_pending()
-    changed = []
     item_list_qty_col = {}
     item_pending_qty_col = {}
     open_order_qty_col = {}
@@ -373,17 +372,18 @@ def adjust_q4s():
             q4s = q4s if q4s > 0 else 0
             q4s = q4s if q4s < 10 else 9
         listing.update_qty(qty)
-        listing.update_q4s(q4s)
+        listing.update_last_q4s(q4s)
         _logger.debug('sku {}, orginal q4s {}, new q4s {}, qty {}'.format(listing.sku, listing.q4s, q4s, qty))
-        if listing.q4s != q4s:
-            listing.append_memo('q4s from {} to {}'.format(listing.q4s, q4s))
-            changed.append(listing)
-            # _logger.debug('item:{} qty:{} q4s:{} pending: {} open order:{}'.format(listing.sku, listing.qty, listing.q4s, pending_qty, open_order_qty))
+        # if listing.q4s != q4s:
+        #     listing.append_memo('q4s from {} to {}'.format(listing.q4s, q4s))
+        #     changed.append(listing)
+        # _logger.debug('item:{} qty:{} q4s:{} pending: {} open order:{}'.format(listing.sku, listing.qty, listing.q4s, pending_qty, open_order_qty))
 
-    _upload_q4s(changed)
+        # _upload_q4s(changed)
 
 
-def _upload_q4s(listing_list):
+def upload_q4s():
+    listing_list = Listing.query.filter_by(status=Listing.STATUS_OPEN, auto_q4s=1)
     if not listing_list:
         return
     with open('inventory_update_template.xml') as fd:
@@ -424,15 +424,17 @@ def _save_listing(listing_from_amazon):
 
 def sync_listing_from_amazon():
     listing_list = _get_listing_data_from_amazon()
+    price_dict = _get_amazon_prices(listing_list)
     for listing in listing_list:
         try:
             listing_in_local = get_listing_by_source_id(source, listing['listing-id'])
             if listing_in_local:
                 kwag = {'q4s': listing['quantity'],
-                        'price': int(float(listing['price']) * 100)}
+                        'price': int(float(price_dict[listing_in_local.sku]) * 100)}
                 # listing_in_local.q4s = listing['quantity']
                 # listing_in_local.price = int(float(listing['price']) * 100)
-                # listing_in_local.last_sync_date = datetime.datetime.now()
+                # listing_in_local.last_sync_date = datetime.datetime.now()]
+                print(kwag)
                 listing_in_local.sync(**kwag)
             else:
                 _insert_single_listing(listing)
@@ -485,15 +487,21 @@ def _get_buy_box_price(html):
     bs = BeautifulSoup(html, 'html.parser')
     price_span = bs.find('span', id='priceblock_ourprice')
     if not price_span:
+        price_span = bs.find('span', id='priceblock_saleprice')
+    if not price_span:
         return None
     seller_div = bs.find('div', id='merchant-info')
     if not seller_div:
         return None
 
     price = price_span.string.strip().replace('$', '')
-    shipping_span = bs.find('span', id='ourprice_shippingmessage').find('span')
+    shipping_span = bs.find('span', id='ourprice_shippingmessage')
     if not shipping_span:
-        # shipping_fee = None
+        shipping_span = bs.find('span', id='saleprice_shippingmessage')
+    if not shipping_span:
+        return None
+    shipping_span = shipping_span.find('span')
+    if not shipping_span:
         return None
     else:
         # shipping_fee = shipping_span.find('span').string.strip().split(' ')[1].replace('$', '')
@@ -510,6 +518,7 @@ def _get_buy_box_price(html):
 
 
 def _parser_shipping_fee(contents):
+    print(contents)
     if not contents:
         return None
     show_free_shipping = False
@@ -517,7 +526,9 @@ def _parser_shipping_fee(contents):
     for c in contents:
         c2 = c.upper()
         if 'OVER' in c2:
-            return None
+            return '0'
+        if 'AMAZON PRIME' in c2:
+            return '0'
         elif 'FREE SHIPPING' in c2:
             show_free_shipping = True
         else:
@@ -533,10 +544,20 @@ def _parser_shipping_fee(contents):
 def sync_competitive_prices():
     listing_list = Listing.query.filter_by(status=Listing.STATUS_OPEN).all()
     for listing in listing_list:
-        buy_box_price, offer_box_prices = _get_listing_prices(listing.source_item_id, listing.listing_url)
-        if offer_box_prices is None:
-            offer_box_prices = ()
-        listing.update_competitive_prices(buy_box_price, *offer_box_prices)
+        # buy_box_price, offer_box_prices = _get_listing_prices(listing.source_item_id, listing.listing_url)
+        # if offer_box_prices is None:
+        #     offer_box_prices = ()
+        # listing.update_competitive_prices(buy_box_price, *offer_box_prices)
+        _download_single_item_prices(listing)
+
+
+def _download_single_item_prices(listing, listing_id=None):
+    if listing_id:
+        listing = Listing.query.filter_by(listing_id=listing_id).one()
+    buy_box_price, offer_box_prices = _get_listing_prices(listing.source_item_id, listing.listing_url)
+    if offer_box_prices is None:
+        offer_box_prices = ()
+    listing.update_competitive_prices(buy_box_price, *offer_box_prices)
 
 
 def calculate_price():
@@ -557,10 +578,14 @@ def calculate_price():
         price = listing.min_price if price < listing.min_price else price
         listing.update_last_price(price)
 
+
 def upload_price():
     listing_list = Listing.query.filter_by(status=Listing.STATUS_OPEN, auto_price=1).all()
     changed = []
     for listing in listing_list:
+        if 'fabuzone' in [listing.buy_box_seller, listing.offer_box_seller_1, listing.offer_box_seller_2,
+                          listing.offer_box_seller_3]:
+            continue
         if listing.last_price is None or listing.price == listing.last_price:
             continue
         changed.append(listing)
@@ -573,6 +598,22 @@ def upload_price():
         _submit_feed('_POST_PRODUCT_PRICING_DATA_', feed_content)
     _logger.info('{} listing(s) uploaded.'.format(len(changed)))
 
+
+def _get_amazon_prices(listing_list):
+    # listing_list = Listing.query.filter_by(status=Listing.STATUS_OPEN)
+    sku_list = [i['seller-sku'] for i in listing_list]
+
+    conn = connection.MWSConnection(Merchant=merchant_id)
+    price_dict = {}
+    for i in range(0, len(sku_list), 20):
+        r = conn.get_my_price_for_sku(MarketplaceId=marketplace_id, SellerSKUList=sku_list[i:i+20])
+        for result in r.GetMyPriceForSKUResult:
+            offer = result.Product.Offers.Offer
+            if offer:
+                price_dict[offer[0].SellerSKU] = offer[0].BuyingPrice.ListingPrice
+    # for listing in listing_list:
+    #     listing.sync(price=int(float(price_dict[listing.sku]) * 100))
+    return price_dict
 
 def init(flask_app, module_path, db_uri, logger):
     sys.path.append(module_path)
@@ -619,16 +660,17 @@ if __name__ == '__main__':
     # refresh_listing_from_amazon()
 
     # print([(i.price, i.shipping, i.seller) for i in _get_listing_prices('B015TP5L4K')])
-    # sync_listing_from_amazon()
+    sync_listing_from_amazon()
     # adjust_q4s()
     # adjust_price()
-    # t1, t2 = _get_listing_prices('B00AKG31SM')
-    # t1, t2 = _get_listing_prices('B00SSSBY7Y')
-    # t1, t2 = _get_listing_prices('B00E1EP4FW')
-    # print(t1.price, t1.shipping, t1.seller)
-    sync_competitive_prices()
+    # sync_competitive_prices()
     # calculate_price()
     # upload_price()
+    # calculate_q4s()
+
+    # buy, offer = _get_listing_prices('B00WG0IX7Y')
+    # print(buy.price, buy.shipping, buy.seller)
+    # _download_single_item_prices(None, 35)
 
     # import sys
     # sys.path.append('../')
